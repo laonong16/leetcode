3a5cf05221ebd0708be3402e962393d539c91590
laonong16 <laonong16@gmail.com>
2017-09-13 23:23:49 -0700

    amazon

---
+++ amazon/63. Unique Paths II.java
@@ -0,0 +1,55 @@
+/*
+63. Unique Paths II
+Follow up for "Unique Paths":
+
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
+
+An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+
+For example,
+There is one obstacle in the middle of a 3x3 grid as illustrated below.
+
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+The total number of unique paths is 2.
+*/
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if(obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0) {
+            return 0;
+        }
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        if(obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+        for(int i = 0; i < m; i++){
+            if(obstacleGrid[i][0] == 1) {
+                break;
+            } else {
+                dp[i][0] = 1;
+            }
+        }
+        for(int j = 0; j < n; j++) {
+            if(obstacleGrid[0][j] == 1) {
+               break;
+            } else {
+                dp[0][j] = 1;
+            }
+        }
+        for(int i = 1; i < m; i++) { 
+            for(int j = 1; j < n; j++) { 
+                if(obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else { 
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
