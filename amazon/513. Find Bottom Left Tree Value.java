fcbd22a76d4223e72afc4925d93969da60461fc9
laonong16 <laonong16@gmail.com>
2017-09-13 23:24:51 -0700

    amazon

--- amazon/513. Find Bottom Left Tree Value.java
+++ amazon/513. Find Bottom Left Tree Value.java
@@ -1,1 +1,61 @@
+/*
 513. Find Bottom Left Tree Value
+Given a binary tree, find the leftmost value in the last row of the tree.
+
+Example 1:
+Input:
+
+    2
+   / \
+  1   3
+
+Output:
+1
+Example 2: 
+Input:
+
+        1
+       / \
+      2   3
+     /   / \
+    4   5   6
+       /
+      7
+
+Output:
+7
+Note: You may assume the tree (i.e., the given root node) is not NULL.
+
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        int lastleft = root.val;
+        Queue<TreeNode> que = new LinkedList<>();
+        que.offer(root);
+        while (!que.isEmpty()) {
+            int size = que.size();
+            for(int i = 0; i < size; i++) {
+                TreeNode node = que.poll();
+                if(i == 0) {
+                    lastleft = node.val;
+                }
+                if(node.left != null) {
+                    que.offer(node.left);
+                }
+                if(node.right != null) {
+                    que.offer(node.right);
+                }
+            }
+        }
+        return lastleft;
+    }
+}
